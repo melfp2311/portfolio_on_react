76904bd1a2086c3377d84bf829229d56cf679445
Melissa Flores <mel.floresp@gmail.com>
2020-08-25 23:03:30 -0500

    commiting changes to deploy on github pages

---
+++ build/precache-manifest.fc7325849f7269c1371d8e27cc919269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ad254d372f133df8ec59b9350e6388",
+    "url": "/portfolio_on_react/index.html"
+  },
+  {
+    "revision": "2db7abb91fd0f40458af",
+    "url": "/portfolio_on_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b7d894523f63ef068c6c",
+    "url": "/portfolio_on_react/static/js/2.928078c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_on_react/static/js/2.928078c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db7abb91fd0f40458af",
+    "url": "/portfolio_on_react/static/js/main.56977bbe.chunk.js"
+  },
+  {
+    "revision": "d735aa547eb197f1958d",
+    "url": "/portfolio_on_react/static/js/runtime-main.6d781ce5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio_on_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
