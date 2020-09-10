d155d0aef346f7df4afb7dd0743599bd92fb646b
Melissa Flores <mel.floresp@gmail.com>
2020-09-10 18:19:23 -0500

    changes to classNames on contact info component

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -5,14 +5,14 @@ import "./styles.css"
 function ContactInfo(){
     return (
 <div className="container">
-<div class="row">
+<div className="row">
       <div class="col-lg-6 col-sm-12">
-      <img src="../../assets/Images/contact.png" class="img-fluid" alt="Responsive image"/>
+      <img src="../../assets/Images/contact.png" className="img-fluid" alt="Responsive image"/>
     </div>
     </div>      
     
-  <div class="row">
-    <div class="col-lg-6 col-sm-12">
+  <div className="row">
+    <div className="col-lg-6 col-sm-12">
           <h5>email.</h5>
           <a href="mailto:mel.floresp@gmail.com">mel.floresp@gmail.com</a>
           <a href="https://www.linkedin.com/in/melissa-flores-prieto-08573b109/">LinkedIn</a>
@@ -20,8 +20,8 @@ function ContactInfo(){
           {/* <!-- //QUOTE HERE --> */}
     </div>
 
-    <div class="col-lg-6 col-sm-12">
-      <ul class="list-unstyled">
+    <div className="col-lg-6 col-sm-12">
+      <ul className="list-unstyled">
         <h5>snail mail.</h5>
         <li>Melissa Flores</li>
         <li>Varsovia 36, Colonia Juárez</li>
